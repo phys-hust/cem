d81572a1278fcfb0b20c9bfa7758bf11092327f2
wxz@hust.edu.cn <wxz@hust.edu.cn>
2020-08-31 19:14:07 +0800

    Make tests more robust

--- tests/test_planewave.py
+++ tests/test_planewave.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+from random import random
 from cem.planewave import C, Space
 
 
@@ -8,22 +9,28 @@ def test_speed_light():
 
 
 def test_space():
-    space = Space(size=(1, 10), step_size=(0.1, 0.01), wvlen=5)
-    assert space._Nx == pytest.approx(1 / 0.1)
-    assert space._Ny == pytest.approx(10 / 0.01)
-    assert space._k == pytest.approx(2 * np.pi / 5)
+    X = random() * 10
+    Y = random() * 10
+    dX = random() / 10
+    dY = random() / 10
+    wvlen = random()
+
+    space = Space(size=(X, Y), step_size=(dX, dY), wvlen=wvlen)
+
+    assert space._Nx == X // dX
+    assert space._Ny == Y // dY
+    assert space._k == pytest.approx(2 * np.pi / wvlen)
     assert space._omega == pytest.approx(C * space._k)
 
-
-def test_prop():
-    wvlen = 5
-    space = Space(size=(1, 10), step_size=(0.1, 1), wvlen=wvlen)
     assert space._E0 == pytest.approx(1)
     assert space._phi == pytest.approx(0)
+
     T0 = wvlen / C
     space.propagate(T0 / 4)
     assert space._Ex[:, 0] == pytest.approx(np.zeros(space._Nx))
     space.propagate(T0 / 2)
     assert space._Ex[:, 0] == pytest.approx(-np.ones(space._Nx))
     space.propagate(T0)
+    assert space._Ex[:, 0] == pytest.approx(np.ones(space._Nx))
+    space.propagate(T0 * 2)
     assert space._Ex[:, 0] == pytest.approx(np.ones(space._Nx))
